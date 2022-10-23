74d1de67db7bd22289f26a15c7488a89a2313647
Antonio <ajenkins2@student.fullsail.edu>
2022-10-22 18:32:12 -0700

    FINISHED :: Header -> Added Search and Mobile Menu

--- airtj/components/Header.js
+++ airtj/components/Header.js
@@ -1,3 +1,10 @@
+import {
+    GlobeAltIcon,
+    MenuIcon,
+    SearchIcon,
+    UserCircleIcon,
+    UserIcon,
+} from '@heroicons/react/solid';
 import Image from 'next/image';
 import React from 'react';
 const Header = () => {
@@ -11,8 +18,23 @@ const Header = () => {
                     objectPosition="left"
                 />
             </div>
-            <div>Search</div>
-            <div>Become a host</div>
+            <div className="flex align-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input
+                    type="text"
+                    placeholder="Start your search"
+                    className="text-gray-500 placeholder-gray-400 pl-5 bg-transparent outline-none flex-grow"
+                />
+                <SearchIcon className="hidden md:inline-flex rounded-full p-2 h-8 cursor-pointer bg-red-400 text-white md:mx-2" />
+            </div>
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
+
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <MenuIcon className="h-6 cursor-pointer" />
+                    <UserCircleIcon className="h-6 cursor-pointer" />
+                </div>
+            </div>
         </header>
     );
 };
