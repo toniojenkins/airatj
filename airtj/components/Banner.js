21c29a237fbacb7572bcab538f94c4d30d50d205
Antonio <ajenkins2@student.fullsail.edu>
2022-10-22 18:54:03 -0700

    FINISHED :: Banner starting main section

--- airtj/components/Banner.js
+++ airtj/components/Banner.js
@@ -1,7 +1,23 @@
+import Image from 'next/image';
 import React from 'react';
-
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+        <div className="relative h-[300px] sm:h-[475px] lg:h-[575px] xl:h-[675px] 2xl:h-[775px]">
+            <Image
+                src="https://links.papareact.com/0fm"
+                layout="fill"
+                objectFit="cover"
+            />
+            <div className="absolute top-1/2 w-full text-center">
+                <p className="text-sm sm:text-lg">
+                    Not sure where to go? Perfect.
+                </p>
+                <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md  font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+                    I'm flexible
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Banner;
--- airtj/pages/index.tsx
+++ airtj/pages/index.tsx
@@ -1,13 +1,24 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 const Home: NextPage = () => {
     return (
         <div className="">
             <Header />
-            {/* <Banner /> */}
+            <Banner />
+
+            <main className="max-w-7xl mx-auto px-8 sm:px-16">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">
+                        Explore Nearby
+                    </h2>
+
+                    {/* Pull some data from a server - API endpoints */}
+                </section>
+            </main>
         </div>
     );
 };
