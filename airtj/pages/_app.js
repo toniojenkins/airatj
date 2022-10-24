f86fffdc98cae380278a2e1dd54ad20a44e9eded
Antonio <ajenkins2@student.fullsail.edu>
2022-10-23 20:07:10 -0700

    ADDED: progressbar loading

--- airtj/pages/_app.js
+++ airtj/pages/_app.js
@@ -1,6 +1,20 @@
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
 import '../styles/globals.css';
+
+const progress = new ProgressBar({
+    size: 5,
+    color: '#FE595E',
+    className: 'z-50',
+    delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 const MyApp = ({ Component, pageProps }) => {
     return <Component {...pageProps} />;
 };
 export default MyApp;
+ß;
